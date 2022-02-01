a39ce2a7f1dc255c93d4c8fd4ee92bf05d11ad71
Justin <ShadowPeo@users.noreply.github.com>
2022-02-01 15:54:58 +1100

    Update
    Update

---
+++ InTune/Proactive Remediation/Uptime/Detect.ps1
@@ -0,0 +1,23 @@
+﻿# Detection
+try {
+
+    $uptime = ((get-date) - (gcim Win32_OperatingSystem).LastBootUpTime).Days
+    
+    # evaluate the compliance
+    if ($uptime -le 7) {
+
+        Write-Host "Uptime is acceptable"
+        exit 0
+    }
+    else {
+        Write-Host "Uptime is greater than a week"
+        exit 1
+    }
+   
+    
+}
+catch {
+    $errMsg = _.Exception.Message
+    Write-Host $errMsg
+    exit 1
+}
--- Network Management/managmentFunctions.psm1
+++ Network Management/managmentFunctions.psm1
@@ -77,20 +77,22 @@ function Convert-MACAddress
     
 }
 
-function addDeviceRecord
+function Register-DeviceRecord
 {
     Param 
     (
     
-        [Parameter(Mandatory=$true)][ipaddress]$deviceIP,
-        [Parameter(Mandatory=$true)][ipaddress]$deviceSubnet,
-        [Parameter(Mandatory=$true)][string]$deviceName,
-        [Parameter(Mandatory=$true)][string]$deviceMAC,
+        [Parameter(Mandatory=$true,HelpMessage="IP Address of the device you want to add a reservation for")][ipaddress]$deviceIP,
+        [Parameter(Mandatory=$true,HelpMessage="Subnet of the device you want to add a reservation for")][ipaddress]$deviceSubnet,
+        [Parameter(Mandatory=$true,HelpMessage="Name of the device you want to add a reservation for")][string]$deviceName,
+        [Parameter(Mandatory=$true,HelpMessage="MAC Address of the device you want to add a reservation for")][string]$deviceMAC,
         [string]$deviceDescription,
         [string]$deviceDomain,
         [ipaddress]$deviceDHCPServer,
         [ipaddress]$deviceDNSServer,
-        [switch]$LeaveExistingDNS
+        [switch]$leaveExistingDNS,
+        [switch]$leaveExistingForwardDNS,
+        [switch]$leaveExistingReverseDNS
     
     )
 
@@ -110,25 +112,60 @@ function addDeviceRecord
 
     if ($deviceDHCPServer -ne "" -and $null -ne $deviceDHCPServer)
     {
-        #Add-DhcpServerv4Reservation -ComputerName $deviceDHCPServer -ScopeId (Get-NetworkAddress $deviceIP $deviceSubnet) -IPAddress $deviceIP -Name "$deviceName.$deviceDomain" -ClientId (Convert-MACAddress $deviceMAC) -Description "`"$deviceDescription`""
+        Add-DhcpServerv4Reservation -ComputerName $deviceDHCPServer -ScopeId (Get-NetworkAddress $deviceIP $deviceSubnet) -IPAddress $deviceIP -Name "$deviceName.$deviceDomain" -ClientId (Convert-MACAddress $deviceMAC) -Description "$deviceDescription"
     }
     else 
     {
-        #Add-DhcpServerv4Reservation -ScopeId (Get-NetworkAddress $deviceIP $deviceSubnet) -IPAddress $deviceIP -Name "$deviceName.$deviceDomain" -ClientId (Convert-MACAddress $deviceMAC) -Description $deviceDescription
+        Add-DhcpServerv4Reservation -ScopeId (Get-NetworkAddress $deviceIP $deviceSubnet) -IPAddress $deviceIP -Name "$deviceName.$deviceDomain" -ClientId (Convert-MACAddress $deviceMAC) -Description "$deviceDescription"
     }
 
-    #Removing Existing DNS if required
-    if (!$LeaveExistingDNS)
+
+    #Removing Existing Forward DNS if required
+    if (!$leaveExistingForwardDNS -and !$leaveExistingDNS)
     {
         try
         {
-            $tempDNS = @(Get-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName $deviceDomain -Name $deviceName)
-            Write-Host $tempDNS
+            $tempDNSForward = @(Get-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName $deviceDomain -Name $deviceName)
 
-            if ($null -ne $tempDNS)
+
+            if ($tempDNSForward.Count -gt 0)
             {
-                Remove-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName $deviceDomain -Name $deviceName -RRType "A" -Force
-                Write-Host "Removing Existing DNS"
+                foreach($forwardRecord in $tempDNSForward)
+                {
+                    Remove-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName $deviceDomain -Name $deviceName -RRType $forwardRecord.RecordType -Force
+                    Write-Host "Removing Forward Record for $deviceName on $((($forwardRecord.RecordData).IPv4Address).IPAddressToString)"
+                }
+            }
+        }
+        catch
+        {
+
+
+        }
+
+    }
+
+    #Removing Existing Reverse DNS if required
+    if (!$leaveExistingReverseDNS -and !$leaveExistingDNS)
+    {
+        try
+        {
+            
+            $ipArray = ($deviceIP.ToString()).split(".")
+            $tempDNSReverse = @(Get-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName ("$($ipArray[2]).$($ipArray[1]).$($ipArray[0]).in-addr.arpa") -Name $ipArray[3])
+
+            if ($tempDNSReverse.Count -gt 0)
+            {
+                $removedReverseRecords = @()
+                foreach($reverseRecord in $tempDNSReverse)
+                {
+                    if ($removedReverseRecords -notcontains $ipArray[3])
+                    {
+                        Remove-DnsServerResourceRecord -ComputerName $deviceDNSServer -ZoneName ("$($ipArray[2]).$($ipArray[1]).$($ipArray[0]).in-addr.arpa") -Name $ipArray[3] -RRType "Ptr" -Force
+                        $removedReverseRecords += $ipArray[3]
+                        Write-Host "Removing Reverse Record for $((($reverseRecord.RecordData).PtrDomainName).Substring(0,(($reverseRecord.RecordData).PtrDomainName).Length-1)) on $deviceIP"
+                    }
+                }
             }
         }
         catch
@@ -151,20 +188,52 @@ function addDeviceRecord
 
 }
 
-<#
-function addPrinter ($printerDNSAddress, $printerDNSName, $printerShareName, $printerLocation, $printerSNMPCommunity, $printerDriver)
+
+function Register-Printer
 {
+    
+    Param 
+    (
+    
+        [Parameter(Mandatory=$true)][ipaddress]$deviceIP,
+        [Parameter(Mandatory=$true)][string]$deviceName,
+        [ipaddress]$deviceSubnet,
+        [string]$deviceMAC,
+        [string]$deviceDescription,
+        [string]$deviceDomain,
+        [ipaddress]$deviceDHCPServer,
+        [ipaddress]$deviceDNSServer,
+        [switch]$leaveExistingDNS,
+        [switch]$leaveExistingForwardDNS,
+        [switch]$leaveExistingReverseDNS,
+        
+        #Printer Specific Information
+        [Parameter(Mandatory=$true)][ipaddress]$devicePrintServer,
+        [Parameter(Mandatory=$true)][string]$printerLocation,
+        [Parameter(Mandatory=$true)][string]$printerSNMPCommunity,
+        [Parameter(Mandatory=$true)][string]$printerDriver,
+        [string]$printerShareName,
+        #[string]$driverFolder,
+        [string]$printerLPRQueue,
+        #[switch]$installDriver,
+        [switch]$printerLPR,
+        [switch]$noDNS = $false,
+        [switch]$noDHCP = $false
+    
+    )
+
+ 
     if (($printerShareName -eq $null)-or($printerShareName -eq ""))
     {
-        $printerShareName = $printerDNSName
+        $printerShareName = $deviceName
     }
-
-    if (($printerSNMPCommunity -eq $null)-or($printerSNMPCommunity -eq ""))
+<#    
+    if (($printerSNMPCommunity -eq $null) -or ($printerSNMPCommunity -eq ""))
     {
         $printerSNMPCommunity = "public"
     }
 
     
     Add-PrinterPort -ComputerName "$sitePrinterServer.$siteDomain" -PrinterHostAddress "$printerDNSAddress.$siteDomain" -SNMP 1 -SNMPCommunity $printerSNMPCommunity -Name $printerDNSName
-    Add-Printer -ComputerName "$sitePrinterServer.$siteDomain" -Name $printerShareName -DriverName $printerDriver -port $printerDNSName -Shared -ShareName $printerShareName –Published  -RenderingMode SSR -Location $printerLocation
-}#>
+    Add-Printer -ComputerName "$sitePrinterServer.$siteDomain" -Name $printerShareName -DriverName $printerDriver -port $printerDNSName -Shared -ShareName $printerShareName –Published  -RenderingMode SSR -Location $printerLocation#>
+}
